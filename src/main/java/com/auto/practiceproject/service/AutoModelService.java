0ae24281a14218f6da92c191bd35ce4168290730
GrabarDmitry <grabar.dimulya@mail.ru>
2022-02-05 16:40:48 +0300

    Edit auto model service test

--- src/main/java/com/auto/practiceproject/service/AutoModelService.java
+++ src/main/java/com/auto/practiceproject/service/AutoModelService.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface AutoModelService {
 
-    public List<AutoModel> findAllAutoModel(String filter);
+    List<AutoModel> findAllAutoModel(String filter);
 
     Optional<AutoModel> findAutoModel(Long id);
 
--- src/test/java/com/auto/practiceproject/service/AutoModelServiceTest.java
+++ src/test/java/com/auto/practiceproject/service/AutoModelServiceTest.java
@@ -1,9 +1,8 @@
 package com.auto.practiceproject.service;
 
 import com.auto.practiceproject.dao.AutoModelDAO;
-import com.auto.practiceproject.model.AutoBrand;
+import com.auto.practiceproject.exception.ResourceException;
 import com.auto.practiceproject.model.AutoModel;
-import com.auto.practiceproject.model.AutoReleasedYear;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,10 +11,15 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.nullable;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,26 +31,76 @@ public class AutoModelServiceTest {
     @MockBean
     private AutoModelDAO autoModelDAO;
 
-//    @Before
-//    public void setUp() {
-//        AutoModel autoModel = new AutoModel("X5", new AutoBrand("BMW"),
-//                new AutoReleasedYear(LocalDate.parse("2020-01-01")));
-//
-//        Mockito.when(autoModelDAO.findAutoModelByTitle("X5"))
-//                .thenReturn(Optional.of(autoModel));
-//    }
-//
-//    @Test
-//    public void findAutoModelByTitleTest() {
-//        Optional<AutoModel> autoModel = autoModelService.findAutoModelByTitle("X5");
-//        Assert.assertTrue(autoModel.isPresent());
-//        Assert.assertEquals(autoModel.get().getTitle(), "X5");
-//    }
-//
-//    @Test
-//    public void findAutoModelByTitleNullIsTest() {
-//        Optional<AutoModel> autoModel = autoModelService.findAutoModelByTitle("X7");
-//        Assert.assertTrue(autoModel.isEmpty());
-//    }
+    private List<AutoModel> autoModelData;
+
+    @Before
+    public void setUp() {
+        autoModelData = new ArrayList<>();
+
+        AutoModel autoModel1 = new AutoModel();
+        autoModel1.setId(1L);
+        AutoModel autoModel2 = new AutoModel();
+        autoModel2.setId(2L);
+        AutoModel autoModel3 = new AutoModel();
+        autoModel3.setId(3L);
+
+        autoModelData.add(autoModel1);
+        autoModelData.add(autoModel2);
+        autoModelData.add(autoModel3);
+    }
+
+    @Test
+    public void findAllAutoModelsTest() {
+        Mockito.when(autoModelDAO.findAll(nullable(Specification.class)))
+                .thenReturn(autoModelData);
+
+        List<AutoModel> autoModels = autoModelService.findAllAutoModel(null);
+
+        Assert.assertEquals(autoModels.size(), 3);
+        Assert.assertEquals(autoModels.get(0).getId().longValue(), 1L);
+        Assert.assertEquals(autoModels.get(1).getId().longValue(), 2L);
+        Assert.assertEquals(autoModels.get(2).getId().longValue(), 3L);
+    }
+
+    @Test
+    public void findAutoModelByIdTest() {
+        Mockito.when(autoModelDAO.findById(1l))
+                .thenReturn(Optional.of(autoModelData.get(0)));
+
+        AutoModel autoModel = autoModelService.findAutoModelById(1L);
+
+        Assert.assertNotNull(autoModel);
+        Assert.assertEquals(autoModel.getId().longValue(), 1L);
+    }
+
+    @Test(expected = ResourceException.class)
+    public void findAutoModelByIdIsNullTest() {
+        AutoModel autoModel = autoModelService.findAutoModelById(3L);
+
+        Assert.assertNull(autoModel);
+
+        Exception exception = assertThrows(
+                ResourceException.class,
+                () -> autoModelService.findAutoModelById(3L));
+        Assert.assertTrue(exception.getMessage().contains(
+                "Auto model with Id: 3 not found"
+        ));
+    }
+
+    @Test
+    public void findAutoModelTest() {
+        Mockito.when(autoModelDAO.findById(1l))
+                .thenReturn(Optional.of(autoModelData.get(0)));
+
+        Optional<AutoModel> autoModel = autoModelService.findAutoModel(1L);
+        Assert.assertTrue(autoModel.isPresent());
+        Assert.assertEquals(autoModel.get().getId().longValue(), 1l);
+    }
+
+    @Test
+    public void findAutoModelIsNullTest() {
+        Optional<AutoModel> autoModel = autoModelService.findAutoModel(3L);
+        Assert.assertTrue(autoModel.isEmpty());
+    }
 
 }
